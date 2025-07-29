c5798e7be32ccbabfceb6055d0ab7aae78345115
LittleViewer <planete.adoecrivain@gmail.com>
2025-07-29 18:15:32 +0200

    Rajout de env.exemple et debut de generateKey

--- index.php
+++ index.php
@@ -1,11 +1,5 @@
 <?php
 
-require_once 'mardukCore/routingUserPage.php';
+require_once 'mardukCore/generateKey.php';
 
-$routingPage = new routingUserPage();
-
-$arrayRoutingCodeExecute = $routingPage->willRedirect(10000);
-
-$routingCodeExcute = $arrayRoutingCodeExecute[0];
-
-$routingCodeExcute($arrayRoutingCodeExecute[1], $arrayRoutingCodeExecute[2]);
+new generateKey("devForUserFold");
